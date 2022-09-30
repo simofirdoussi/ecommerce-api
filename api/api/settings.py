f539342d0f4feb9352e9fca9d440387d0e3c693a
Mohamed <mohamed.elfirdoussi@um5r.ac.ma>
2022-09-30 11:24:27 +0100

    add swagger documentation

--- api/api/settings.py
+++ api/api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -127,5 +128,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
 
 AUTH_USER_MODEL = 'core.User'
